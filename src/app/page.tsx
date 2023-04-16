dee27f37c22727fcd744b9bca332d58f6ba1f51b
Noor Muhammad <connectwithnoor1@gmail.com>
2023-04-16 20:00:13 +0500

    readme-updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,4 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
 import styles from "./page.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -10,9 +6,7 @@ export default function Home() {
       <div className={styles.description}>
         <p>Panaverse Ninja Task</p>
         <p>
-          Head to
-          <code className={styles.code}>/api/</code>
-          to find apis
+          Head to <code className={styles.code}>/api/status</code> to find apis
         </p>
       </div>
     </main>
